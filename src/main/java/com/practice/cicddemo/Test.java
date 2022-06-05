2064d0ec6e8a775b45aa4923c1f7b4cde6f98c5b
kalyanmac <kalyan.thota2@gmail.com>
2022-06-05 10:00:28 +0530

    Second Commit to verify jenkins integration

---
+++ src/main/java/com/practice/cicddemo/Test.java
@@ -0,0 +1,8 @@
+package com.practice.cicddemo;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("************ I am From Test Class ************");
+    }
+}
