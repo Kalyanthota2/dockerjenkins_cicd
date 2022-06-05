186094999c56a802a7aa727090c31d8b3393db88
Kalyan Mac <kalyanmac@Kalyans-MacBook-Air.local>
2022-06-05 09:43:16 +0530

    added console log

--- src/main/java/com/practice/cicddemo/CicddemoApplication.java
+++ src/main/java/com/practice/cicddemo/CicddemoApplication.java
@@ -8,6 +8,7 @@ public class CicddemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CicddemoApplication.class, args);
+		System.out.println("+++++ Hi I am From CICD Demo Project +++++");
 	}
 
 }
