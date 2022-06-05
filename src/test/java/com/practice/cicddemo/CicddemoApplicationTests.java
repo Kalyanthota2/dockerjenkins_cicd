225468c0d83a2a03a8017c5f78c246d77b17c471
Kalyan Mac <kalyanmac@Kalyans-MacBook-Air.local>
2022-06-05 09:38:48 +0530

    first commit

---
+++ src/test/java/com/practice/cicddemo/CicddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
